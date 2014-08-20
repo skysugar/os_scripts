dbfb228a0280da14e70de8506f7c3cb2c3c72448
skysugar <iskysugar@gmail.com>
2014-08-20 14:32:56 +0800

    Create fastping.sh

---
+++ fastping.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+toping(){
+  ping -c 1 -w 1 -q $1 > /dev/null
+  [ $? -eq 0 ] && echo $1 is online...
+}
+
+ip=${1%.*}
+
+for i in $(seq 1 $2 254)
+do
+  n=$i
+  while [ $n -lt $((i+$2)) ]
+  do
+      toping $ip.$n &
+      n=$((n+1))
+      [ $n -ge 255 ] && break
+  done
+  wait
+done
